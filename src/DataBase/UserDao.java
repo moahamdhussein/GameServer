e34154c202780dae6e46bcdcd9790948d79136e5
moahmed hussein <mh95568@gmail.com>
2023-12-26 17:33:06 +0200

    fix bugs

---
+++ src/DataBase/UserDao.java
@@ -0,0 +1,11 @@
+package DataBase;
+
+import java.sql.ResultSet;
+
+public interface UserDao {
+    void getUserData(int Id , DaoCallback<ResultSet> callback);
+    void getDataForLogin(String email, DaoCallback<ResultSet> callback);
+    void addNewUser(int id,DaoCallback<Integer> callback);
+    void updateScore(int id , int isWin , DaoCallback<Integer> callback);
+    void getAllPlayer(DaoCallback<ResultSet> callback);
+}
