4ae7c8d1c04f0299d5859ad473759f20e3ea0522
Israa Assem <israaassem20@gmail.com>
2023-12-27 03:26:48 +0200

    setup json connection

---
+++ src/request/LoginRequest.java
@@ -0,0 +1,27 @@
+
+package request;
+
+/**
+ *
+ * @author esraa
+ */
+public class LoginRequest {
+    private String name;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/request/NetworkRequest.java
@@ -0,0 +1,17 @@
+package request;
+
+/**
+ *
+ * @author esraa
+ */
+public class NetworkRequest {
+    RequestType requestType;
+    Object requestData;
+    public Object getRequestData() {
+        return requestData;
+    }
+    public RequestType getRequestType() {
+    return requestType;
+    }
+    
+}
